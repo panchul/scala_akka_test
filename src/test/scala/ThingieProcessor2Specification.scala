cbba5f323b72e14734647de48b4ae8a846d2fcbe
Aleksandr Panchul <aleksandr.panchul@accenture.com>
2017-01-30 11:38:21 -0500

    wip. Still does not work

--- src/test/scala/ThingieProcessor2Specification.scala
+++ src/test/scala/ThingieProcessor2Specification.scala
@@ -1,5 +1,6 @@
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKitBase}
+import akka.testkit.TestActorRef
 import com.typesafe.config.ConfigFactory
 import org.scalacheck._
 import org.specs2.ScalaCheck
@@ -14,12 +15,12 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class ThingieProcessor2Specification extends Specification
 	with ThrownExpectations
 	with ScalaCheck
+  // with org.specs2.time.NoTimeConversions
 {
 //	def is = s2"""
 // Test thingie2 test1 $testThingieWorks
 //  Test thingie2 test2 $testThingieWorks2
 //"""
-
 	def is = s2"""
   Test thingie2 test2 $testThingieWorks2
 """
@@ -28,39 +29,40 @@ class ThingieProcessor2Specification extends Specification
 //  Test thingie2 test1 $testThingieWorks
 //"""
 
-	def genInt = Gen.oneOf(Seq(1,2,3,4,5,6,7,8,9,0))
-  var didTheThingie = 0
 	implicit def arbitraryAkka: Arbitrary[Akka] =
 		Arbitrary(Gen.const(Akka()))
 
-	var myActor: ActorRef = null
-
-	val testThingieWorks = prop { (i: Int, akka: Akka) =>
-		import akka._; akka {
-			within(5.seconds)
-		  {
-				val demo =
-				// keeps failing
-				// 	system.actorSelection("test-actorg").resolveOne(5.second).onComplete{
-				//		case Success(actorRef) => actorRef
-				//		case Failure(ex) => system.actorOf(Props[ThingieProcessor], "test-actorg")
-				//	}
-				if(didTheThingie == 0) {
-					didTheThingie = 1
-					myActor = system.actorOf(Props[ThingieProcessor], "test-actorg1")
-					myActor
-				} else {
-					myActor
-				}
-
-				demo ! "message"
-
-				println(s"""[TRACE] Gen TestKit: i is $i """)
-				println(s"""[TRACE] Gen TestKit: demo is $demo """)
-				ok
-		  }
-		}
-	}.setGen1(genInt)
+//	def genInt = Gen.oneOf(Seq(1,2,3,4,5,6,7,8,9,0))
+//  var didTheThingie = 0
+//
+//	var myActor: ActorRef = null
+//
+//	val testThingieWorks = prop { (i: Int, akka: Akka) =>
+//		import akka._; akka {
+//			within(5.seconds)
+//		  {
+//				val demo =
+//				// keeps failing
+//				// 	system.actorSelection("test-actorg").resolveOne(5.second).onComplete{
+//				//		case Success(actorRef) => actorRef
+//				//		case Failure(ex) => system.actorOf(Props[ThingieProcessor], "test-actorg")
+//				//	}
+//				if(didTheThingie == 0) {
+//					didTheThingie = 1
+//					myActor = system.actorOf(Props[ThingieProcessor], "test-actorg1")
+//					myActor
+//				} else {
+//					myActor
+//				}
+//
+//				demo ! "message"
+//
+//				println(s"""[TRACE] Gen TestKit: i is $i """)
+//				println(s"""[TRACE] Gen TestKit: demo is $demo """)
+//				ok
+//		  }
+//		}
+//	}.setGen1(genInt)
 
 	val thingieGen: Gen[(String, String)] = Gen.oneOf(Seq(("hi", "hi dear")))
 	var didTheThingie2 = 0
@@ -68,9 +70,9 @@ class ThingieProcessor2Specification extends Specification
 
 	val testThingieWorks2 = prop { (t:Tuple2[String,String], akka: Akka) =>
 		import akka._ ; akka {
-			within(5.seconds)
+			within(20.seconds)
 			{
-				val demo2=
+				val demo=
 					if(didTheThingie2 == 0) {
 						didTheThingie2 = 1
 						myActor2 = system.actorOf(Props[ThingieProcessor], "test-actorg")
@@ -79,21 +81,21 @@ class ThingieProcessor2Specification extends Specification
 						myActor2
 					}
 
-				val demo= system.actorOf(Props[ThingieProcessor], "test-actorg2")
+				//val demo= system.actorOf(Props[ThingieProcessor], "test-actorg")
 
 				println(s"""[TRACE] Gen TestKit: demo is $demo""")
 				val msg_send = t._1 //"hi"
 				val msg_recv = t._2 //"hi dear"
 
-				demo! msg_send
+				//demo! msg_send
 				//demo ! (msg_send)(testActor)
-				//send(demo, msg_send)
+				send(demo, msg_send)
 
 				println(s"""[TRACE] Gen TestKit: sent \"$msg_send\" to $demo""")
 
 				println(s"""[TRACE] Gen TestKit: expecting \"$msg_recv\"... """)
 
-				expectMsg(msg_recv)
+				expectMsg(10.second, msg_recv)
 
 				println(s"""[TRACE] Gen TestKit: after expectMsg """)
 
@@ -122,4 +124,6 @@ case class Akka() extends org.specs2.specification.After with TestKitBase with I
 			system.terminate()
 			//()
 		}
+
+	def send(actor: ActorRef, msg: Any): Unit = actor.!(msg)(testActor)
 }
